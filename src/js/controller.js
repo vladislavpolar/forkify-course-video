d197afe92fcb9a4993e73f7e97e71bad8c5eae04
vladislavpolar <vlad.tsybuliak.vt@gmail.com>
2023-02-15 15:12:32 +0100

    Cleaned something

--- src/js/controller.js
+++ src/js/controller.js
@@ -134,9 +134,6 @@ const controlAddRecipe = async function (newRecipe) {
 
 }
 
-const newFeature = function () {
-  console.log('Welcome to new branch');
-}
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks)
@@ -146,7 +143,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults)
   paginationView.addHandlerClick(controlPagination)
   addRecipeView.addHandlerUpload(controlAddRecipe)
-  newFeature()
 }
 init()
 
